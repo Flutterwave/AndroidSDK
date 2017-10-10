efc0496c0608235e6b763ec12f2cfc61f791474b
Hamza <hfetuga@gmail.com>
2017-10-10 14:22:18 +0100

    update the readme

--- raveandroid/src/main/java/com/flutterwave/raveandroid/RaveConstants.java
+++ raveandroid/src/main/java/com/flutterwave/raveandroid/RaveConstants.java
@@ -7,10 +7,10 @@ package com.flutterwave.raveandroid;
 public class RaveConstants {
     public static int PERMISSIONS_REQUEST_READ_PHONE_STATE = 419;
 
-    public static String PUBLIC_KEY = "";
-    public static String PRIVATE_KEY = "";
-//    public static String PUBLIC_KEY = "FLWPUBK-e634d14d9ded04eaf05d5b63a0a06d2f-X"; //test
-//    public static String PRIVATE_KEY = "FLWSECK-bb971402072265fb156e90a3578fe5e6-X"; //test
+//    public static String PUBLIC_KEY = "";
+//    public static String PRIVATE_KEY = "";
+    public static String PUBLIC_KEY = "FLWPUBK-e634d14d9ded04eaf05d5b63a0a06d2f-X"; //test
+    public static String PRIVATE_KEY = "FLWSECK-bb971402072265fb156e90a3578fe5e6-X"; //test
     public static String STAGING_URL = "http://flw-pms-dev.eu-west-1.elasticbeanstalk.com";
     public static String LIVE_URL = "https://rave-api.herokuapp.com";
     public static String VBV = "VBVSECURECODE";
