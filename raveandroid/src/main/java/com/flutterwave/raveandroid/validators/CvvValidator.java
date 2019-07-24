10bd887cc10c1f53542d3ea1faf92a7f8aecd369
taiwoadebayo <mikelis135@gmail.com>
2019-07-24 12:28:31 +0100

    cvvValidatorTest

--- raveandroid/src/main/java/com/flutterwave/raveandroid/validators/CvvValidator.java
+++ raveandroid/src/main/java/com/flutterwave/raveandroid/validators/CvvValidator.java
@@ -3,6 +3,13 @@ package com.flutterwave.raveandroid.validators;
 public class CvvValidator {
 
     public boolean isCvvValid(String cvv){
-        return cvv.length() >= 3;
+
+        try{
+            Integer.parseInt(cvv);
+            return cvv.length() == 3 || cvv.length() == 4;
+        }catch (NumberFormatException e){
+            return false;
+        }
+
     }
 }
