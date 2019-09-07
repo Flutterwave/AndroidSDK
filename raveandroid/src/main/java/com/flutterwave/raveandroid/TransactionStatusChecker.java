aed0c809e8b3097f1d686d519c0995d2bd895b49
Hamza <hfetuga@gmail.com>
2019-09-07 02:13:29 +0100

    replace org.json.JSONObject with gson's JsonObject this was done to remove dependency on Android and make unit tests possible and easy

--- raveandroid/src/main/java/com/flutterwave/raveandroid/TransactionStatusChecker.java
+++ raveandroid/src/main/java/com/flutterwave/raveandroid/TransactionStatusChecker.java
@@ -1,33 +1,35 @@
 package com.flutterwave.raveandroid;
 
-import org.json.JSONObject;
+import com.google.gson.Gson;
+import com.google.gson.JsonObject;
 
 import javax.inject.Inject;
 
 public class TransactionStatusChecker {
 
+    private Gson gson;
+
     @Inject
-    public TransactionStatusChecker() {
+    public TransactionStatusChecker(Gson gson) {
+        this.gson = gson;
     }
 
-    private static boolean areAmountsSame(String amount1, String amount2) {
+    private boolean areAmountsSame(String amount1, String amount2) {
         Double number1 = Double.parseDouble(amount1);
         Double number2 = Double.parseDouble(amount2);
 
         return Math.abs(number1 - number2) < 0.0001;
     }
 
-    public Boolean getTransactionStatus(RavePayInitializer ravePayInitializer, String responseAsJSONString) {
-        String amount = ravePayInitializer.getAmount() + "";
-        String currency = ravePayInitializer.getCurrency();
+    public Boolean getTransactionStatus(String amount, String currency, String responseAsJSONString) {
 
         try {
-            JSONObject jsonObject = new JSONObject(responseAsJSONString);
-            JSONObject jsonData = jsonObject.getJSONObject("data");
-            String status = jsonData.getString("status");
-            String txAmount = jsonData.getString("amount");
-            String txCurrency = jsonData.getString("currency");
-            String chargeResponse = jsonData.getString("chargeResponseCode");
+            JsonObject jsonObject = gson.fromJson(responseAsJSONString, JsonObject.class);
+            JsonObject jsonData = jsonObject.getAsJsonObject("data");
+            String status = jsonData.get("status").getAsString();
+            String txAmount = jsonData.get("amount").getAsString();
+            String txCurrency = jsonData.get("currency").getAsString();
+            String chargeResponse = jsonData.get("chargeResponseCode").getAsString();
 
             if (areAmountsSame(amount, txAmount) &&
                     chargeResponse.equalsIgnoreCase("00") &&
@@ -36,6 +38,7 @@ public class TransactionStatusChecker {
                     currency.equalsIgnoreCase(txCurrency)) {
                 return true;
             }
+
         } catch (Exception e) {
             return false;
         }
