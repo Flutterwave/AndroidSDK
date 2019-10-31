badc573a3a60cc0b68603b0573374aff8903f073
Jeremiah Ifeanyichukwu <varisiv@gmail.com>
2019-10-31 08:45:42 +0100

    RavePayManager Tests

--- raveandroid/src/test/java/com/flutterwave/raveandroid/RavePayManagerTest.java
+++ raveandroid/src/test/java/com/flutterwave/raveandroid/RavePayManagerTest.java
@@ -4,6 +4,18 @@ import org.junit.Test;
 
 import java.util.ArrayList;
 
+import static com.flutterwave.raveandroid.RaveConstants.PAYMENT_TYPE_ACCOUNT;
+import static com.flutterwave.raveandroid.RaveConstants.PAYMENT_TYPE_ACH;
+import static com.flutterwave.raveandroid.RaveConstants.PAYMENT_TYPE_BANK_TRANSFER;
+import static com.flutterwave.raveandroid.RaveConstants.PAYMENT_TYPE_CARD;
+import static com.flutterwave.raveandroid.RaveConstants.PAYMENT_TYPE_FRANCO_MOBILE_MONEY;
+import static com.flutterwave.raveandroid.RaveConstants.PAYMENT_TYPE_GH_MOBILE_MONEY;
+import static com.flutterwave.raveandroid.RaveConstants.PAYMENT_TYPE_MPESA;
+import static com.flutterwave.raveandroid.RaveConstants.PAYMENT_TYPE_RW_MOBILE_MONEY;
+import static com.flutterwave.raveandroid.RaveConstants.PAYMENT_TYPE_UG_MOBILE_MONEY;
+import static com.flutterwave.raveandroid.RaveConstants.PAYMENT_TYPE_UK;
+import static com.flutterwave.raveandroid.RaveConstants.PAYMENT_TYPE_USSD;
+import static com.flutterwave.raveandroid.RaveConstants.PAYMENT_TYPE_ZM_MOBILE_MONEY;
 import static org.junit.Assert.assertNotEquals;
 
 public class RavePayManagerTest {
@@ -61,6 +73,45 @@ public class RavePayManagerTest {
                             paymentTypesList.get(secondLoopIndex));
                 }
             }
+        }
+    }
+
+
+    @Test
+    public void ravePayManagerSetup_AllPaymentTypesAddedAreInPaymentTypesList() {
+        RavePayManager manager = new RavePayManager(null);
+        manager.acceptCardPayments(true)
+                .acceptAccountPayments(true)
+                .acceptAchPayments(true)
+                .acceptBankTransferPayments(true)
+                .acceptFrancMobileMoneyPayments(true)
+                .acceptGHMobileMoneyPayments(true)
+                .acceptMpesaPayments(true)
+                .acceptRwfMobileMoneyPayments(true)
+                .acceptUgMobileMoneyPayments(true)
+                .acceptUssdPayments(true)
+                .acceptZmMobileMoneyPayments(true)
+                .acceptUkPayments(true);
+
+        ArrayList<Integer> expectedPaymentTypesList = new ArrayList<Integer>() {{
+            add(PAYMENT_TYPE_CARD);
+            add(PAYMENT_TYPE_ACCOUNT);
+            add(PAYMENT_TYPE_ACH);
+            add(PAYMENT_TYPE_BANK_TRANSFER);
+            add(PAYMENT_TYPE_FRANCO_MOBILE_MONEY);
+            add(PAYMENT_TYPE_GH_MOBILE_MONEY);
+            add(PAYMENT_TYPE_MPESA);
+            add(PAYMENT_TYPE_RW_MOBILE_MONEY);
+            add(PAYMENT_TYPE_UG_MOBILE_MONEY);
+            add(PAYMENT_TYPE_USSD);
+            add(PAYMENT_TYPE_ZM_MOBILE_MONEY);
+            add(PAYMENT_TYPE_UK);
+        }};
+
+        ArrayList<Integer> actualPaymentTypesList = manager.getOrderedPaymentTypesList();
+
+        for (int index = 0; index < expectedPaymentTypesList.size(); index++) {
+            assert (actualPaymentTypesList.contains(expectedPaymentTypesList.get(index)));
         }
     }
 
