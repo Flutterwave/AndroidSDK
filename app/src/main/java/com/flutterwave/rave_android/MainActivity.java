578ab9837c64e9a44229d14c91b2a638a84ae1d6
taiwoadebayo <mikelis135@gmail.com>
2020-06-28 17:56:59 +0100

    adjusted colors

--- app/src/main/java/com/flutterwave/rave_android/MainActivity.java
+++ app/src/main/java/com/flutterwave/rave_android/MainActivity.java
@@ -16,6 +16,7 @@ import android.widget.Toast;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatDelegate;
 import androidx.appcompat.widget.SwitchCompat;
 
 import com.flutterwave.raveandroid.RavePayActivity;
@@ -104,6 +105,8 @@ public class MainActivity
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
 
         emailEt = findViewById(R.id.emailEt);
         amountEt = findViewById(R.id.amountEt);
