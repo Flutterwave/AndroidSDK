498d887854d7b98581c84000110004fe15970191
taiwoadebayo <mikelis135@gmail.com>
2020-11-09 17:25:42 +0100

    adjusted set result backstack

--- rave_android/src/main/java/com/flutterwave/raveandroid/data/Utils.java
+++ rave_android/src/main/java/com/flutterwave/raveandroid/data/Utils.java
@@ -114,7 +114,7 @@ public class Utils {
 
             if (activity != null){
                 fragment.getParentFragmentManager().setFragmentResult(RAVE_REQUEST_KEY, bundle);
-                fragment.getParentFragmentManager().popBackStack();
+//                fragment.getParentFragmentManager().popBackStack();
             }
 
         }else{
--- rave_utils/src/main/java/com/flutterwave/raveutils/verification/OTPFragment.java
+++ rave_utils/src/main/java/com/flutterwave/raveutils/verification/OTPFragment.java
@@ -179,7 +179,6 @@ public class OTPFragment extends Fragment implements View.OnClickListener {
             bundle.putInt(RESULT_CODE, RESULT_SUCCESS);
             bundle.putBoolean(IS_SAVED_CARD_CHARGE, isSavedCardCharge);
             getParentFragmentManager().setFragmentResult(VERIFICATION_REQUEST_KEY, bundle);
-//            getParentFragmentManager().popBackStack();
 
         } else {
             Intent intent = new Intent();
